8d5cb54dc4f4e2b821a23bfe01646f6317af58b4
Kosogov <i.kosogov@gmail.com>
2018-11-27 11:05:23 +0200

    Add GoodDetail form

---
+++ UserInterace/Forms/GoodDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserInterace.Forms
+{
+    public partial class GoodDetails : Form
+    {
+        public GoodDetails()
+        {
+            InitializeComponent();
+
+        }
+
+        private void GoodDetails_Load(object sender, EventArgs e)
+        {
+            this.dataGridView1.DataSource = post;
+        }
+    }
+}
